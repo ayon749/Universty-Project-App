fbf9f34902eef97add03a2863747d094fbfa5ecb
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-02-04 20:10:16 +0600

    Department Controller added, data annotation done

--- UniversityProject/UniversityProject/Models/Department.cs
+++ UniversityProject/UniversityProject/Models/Department.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace UniversityProject.Models
 {
@@ -10,9 +11,13 @@ namespace UniversityProject.Models
 	{
 		[Key]
 		public int DepartmentId { get; set; }
+		[Display(Name ="Code")]
 		[Required(ErrorMessage ="Please enter the code")]
 		[StringLength(7,MinimumLength =2,ErrorMessage ="Code must be between 2 to 7 characters")]
+		[Remote("IsCodeExits", "Departments",HttpMethod ="POST",ErrorMessage ="Code already exist.")]
 		public string DepartmentCode { get; set; }
+
+		[Display(Name ="Name")]
 		[Required(ErrorMessage = "Please enter the name")]
 		public string DepartmentName { get; set; }
 	}
