6d7a5abcaf9b418df71ff52e70c02a59c09da556
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-02-04 19:44:30 +0600

    department model,database,department table,1st migration added

---
+++ UniversityProject/UniversityProject/Migrations/202002041342109_DepartmentV1.cs
@@ -0,0 +1,27 @@
+﻿namespace UniversityProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DepartmentV1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        DepartmentId = c.Int(nullable: false, identity: true),
+                        DepartmentCode = c.String(nullable: false, maxLength: 7),
+                        DepartmentName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.DepartmentId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Departments");
+        }
+    }
+}
---
+++ UniversityProject/UniversityProject/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UniversityProject.Models
+{
+	public class Department
+	{
+		[Key]
+		public int DepartmentId { get; set; }
+		[Required(ErrorMessage ="Please enter the code")]
+		[StringLength(7,MinimumLength =2,ErrorMessage ="Code must be between 2 to 7 characters")]
+		public string DepartmentCode { get; set; }
+		[Required(ErrorMessage = "Please enter the name")]
+		public string DepartmentName { get; set; }
+	}
+}
