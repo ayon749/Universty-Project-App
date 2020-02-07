dbe0ffe077faa57cd54a7fa1a5ebdbb374dd32b6
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-02-07 11:38:06 +0600

    student controller student view action created

---
+++ UniversityProject/UniversityProject/Migrations/202002070452427_StudentV2.cs
@@ -0,0 +1,20 @@
+﻿namespace UniversityProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StudentV2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Students", "DepartmentId");
+            AddForeignKey("dbo.Students", "DepartmentId", "dbo.Departments", "DepartmentId", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Students", "DepartmentId", "dbo.Departments");
+            DropIndex("dbo.Students", new[] { "DepartmentId" });
+        }
+    }
+}
--- UniversityProject/UniversityProject/Models/Student.cs
+++ UniversityProject/UniversityProject/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,14 +11,19 @@ namespace UniversityProject.Models
 	{
 		[Key]
 		public int StudentId { get; set; }
+		[Display(Name ="Name")]
 		public string StudentName { get; set; }
 		public string RegistrationNo { get; set; }
 		[Required(ErrorMessage = "Please enter email address.")]
 		[EmailAddress(ErrorMessage = "Invalid email address")]
 		public string Email { get; set; }
+		[Display(Name ="Contact No.")]
 		public string ContactNo { get; set; }
+		[Display(Name ="Date")]
 		public DateTime RegistrationDate { get; set; }
 		public string Address { get; set; }
 		public int DepartmentId { get; set; }
+		[ForeignKey("DepartmentId")]
+		public virtual Department Department { get; set; }
 	}
 }
